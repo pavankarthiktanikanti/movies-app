d7720e9ff4c8d2aeed52538292cc6aa2d29dfc8f
pavankarthiktanikanti <pavankarthiktanikanti@gmail.com>
2020-01-15 17:30:32 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,6 +5,10 @@ import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
 const customStyles = {
     content: {
@@ -16,6 +20,15 @@ const customStyles = {
         transform: 'translate(-50%, -50%)'
     }
 }
+
+const TabContainer = function (props) {
+    return (
+        <Typography component="div" style={{ padding: 0 }} >
+            {props.children}
+        </Typography>
+    );
+}
+
 class Header extends Component {
     constructor() {
         super();
@@ -50,6 +63,16 @@ class Header extends Component {
                         <Tab label="Login" />
                         <Tab label="Register" />
                     </Tabs>
+                    <TabContainer>
+                        <FormControl required>
+                            <InputLabel htmlFor="userName">Username</InputLabel>
+                            <Input id="userName" type="text" />
+                        </FormControl>
+                        <FormControl required>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" type="password" />
+                        </FormControl>
+                    </TabContainer>
                 </Modal>
             </div>
         )
